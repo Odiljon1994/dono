8c8a38bf97959b3ec81a2be018e40c6f8255d607
Ergashev Odiljon <oaergashev@gmail.com>
2024-05-07 01:17:12 +0500

    -minor changes

---
+++ app/src/main/java/com/mcs/wallet/ui/SentFailureActivity.java
@@ -0,0 +1,28 @@
+package com.mcs.wallet.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.mcs.wallet.R;
+import com.mcs.wallet.databinding.ActivitySentFailureBinding;
+
+public class SentFailureActivity extends AppCompatActivity {
+
+    private ActivitySentFailureBinding binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sent_failure);
+
+        binding.okBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        });
+    }
+}
